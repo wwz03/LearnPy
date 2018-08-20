9eac37b921ac87f14477fe519baecc1aec453560
wwz <1195224293@qq.com>
2018-08-21 00:08:34 +0800

    learn coroutine

--- www/python-learn2.py
+++ www/python-learn2.py
@@ -39,3 +39,33 @@ def yhTriangle(n):
 		yield l
 		l = [1] + [l[i] + l[i + 1] for i in range(len(l) - 1)] + [1]
 		index += 1
+
+
+
+#=================================================================================
+#协程学习 coroutine
+#子程序通过栈来实现，一个入口，一个返回，调用顺序明确
+#协程看起来像个子程序，在子程序中中断，去执行其他子程序，python中协程通过generator实现
+#----Python的yield不但可以返回一个值，它还可以接收调用者发出的参数，使用协程实现“生产者-消费者”模型
+
+def consumer():
+	r=''
+	while True:
+		n = yield r #每次只执行一次
+		if not n:
+			return
+		print('[consumer] consumering %s', n)
+		r = '200 OK'
+
+def produce(c):
+	c.send(None)
+	n=0
+	while n<5:
+		n=n+1
+		print('[procuce] procucing %s', n)
+		r = c.send(n) #主动调用协程去执行，并传入参数
+		print('[procuce] consumering return %s', r)
+	c.close()
+
+c = consumer()
+produce(c)
